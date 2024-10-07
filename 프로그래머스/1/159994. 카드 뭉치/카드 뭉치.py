aeeeb7f0f8a0bd91df6a63ccb0ceaa60ee40d857
조승혁 <157351643+J-sh1@users.noreply.github.com>
2024-10-07 17:45:12 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -2,11 +2,13 @@ def solution(cards1, cards2, goal):
     cnt1, cnt2 = 0, 0 
 
     for word in goal:
-        if cnt1 < len(cards1) and cards1[cnt1] == word:  # cards1에서 단어를 사용할 수 있는지 확인
+        if cnt1 < len(cards1) and cards1[cnt1] == word:
             cnt1 += 1
-        elif cnt2 < len(cards2) and cards2[cnt2] == word:  # cards2에서 단어를 사용할 수 있는지 확인
+        elif cnt2 < len(cards2) and cards2[cnt2] == word:
             cnt2 += 1
         else:
-            return "No"  # 둘 중 어느 카드 뭉치에서도 단어를 사용할 수 없다면 No 반환
+            return "No"
 
-    return "Yes"  # 모든 단어를 순서대로 사용할 수 있었다면 Yes 반환
+    return "Yes"
+
+# cards1[cnt1] == word cnt를 + 하면서 검사하는데 배열안에 값을 제거하는식으로도 가능
